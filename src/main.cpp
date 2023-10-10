05360ef035d7bf62e6db3ca893828cb43f7d96b0
Vincent <vafdaf11@gmail.com>
2023-10-10 15:51:10 +0200

    Set up project structure using Google Test

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "multiply/multiply.h"
+#include <iostream>
+
+int main() {
+    std::cout << "COS 214 - Final Project" << std::endl;
+    std::cout << "7 * 6 = " << multiply(7, 6) << std::endl;
+}
---
+++ src/multiply/multiply.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int multiply(int a, int b);
+
+float multiply(float a, float b);
+
+double multiply(double a, double b);
