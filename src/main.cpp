aa9313e2323aba954f095f8fb4d06c476cb54317
Vincent <vafdaf11@gmail.com>
2023-10-19 21:53:45 +0200

    Added custom target for copying assets

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "SFML/Graphics/RectangleShape.hpp"
 #include "gui/Drawable.h"
 #include "multiply/multiply.h"
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "SFML/Graphics.hpp"
@@ -17,9 +19,23 @@ private:
     sf::RectangleShape rect;
 };
 
+void readAssetFile(const std::string& path) {
+    std::ifstream file("assets/" + path);
+    if (!file.is_open()) {
+        std::cout << "Cannot open asset" << std::endl;
+        return;
+    }
+    std::string line;
+    do {
+        std::getline(file, line);
+        std::cout << line << std::endl;
+    } while (!file.eof());
+}
+
 int main() {
     std::cout << "COS 214 - Final Project" << std::endl;
     std::cout << "7 * 6 = " << multiply(7, 6) << std::endl;
+    readAssetFile("demo_asset.json");
     sf::RenderWindow w(sf::VideoMode(800, 600), "COS 214 Final Project");
 
     sf::RectangleShape r({300, 200});
