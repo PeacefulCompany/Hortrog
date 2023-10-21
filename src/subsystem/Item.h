4d5da2836dfe9f20f348e9110f993d2efcb2b5fb
u21434809 <u21434809@tuks.co.za>
2023-10-21 13:57:38 +0200

    added some doxygen

--- src/subsystem/Item.h
+++ src/subsystem/Item.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "MenuItem.h"
-
+/**
+ * @brief a leaf in the composite pattern
+ *
+ */
 class Item : public MenuItem {
 public:
+    /**
+     * @brief Construct a new Item object
+     *
+     * @param name
+     * @param price
+     */
     Item(std::string name, double price);
 private:
 };
--- src/subsystem/Table.h
+++ src/subsystem/Table.h
@@ -1,11 +1,31 @@
 #pragma once
 #include "Order.h"
+/**
+ * @brief a leaf in the composite pattern
+ *
+ */
 class Table : public Order {
 public:
+    /**
+     * @brief Construct a new Item object
+     *
+     * @param name
+     * @param price
+     */
+    Table(std::string name, double price);
+    /**
+     * @brief gets the cost of the item
+     *
+     */
     double getCost();
+    /**
+     * @brief sets the cost of the item
+     *
+     * @param price
+     */
     std::string getJson();
     void add(Order* order);
-    Table(std::string name, double price);
+
     ~Table();
 
 private:
