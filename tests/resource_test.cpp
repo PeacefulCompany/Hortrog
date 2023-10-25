56d9f37b7712c91313ad829776d23bb5809c9a40
Vincent <vafdaf11@gmail.com>
2023-10-25 07:11:37 +0200

    Renamed DemoResource and added additional assertions

--- tests/resource_test.cpp
+++ tests/resource_test.cpp
@@ -1,21 +1,27 @@
 #include <gtest/gtest.h>
 #include <resource/ResourceManager.h>
 
-struct DemoResource {
-    bool loadFromFile(const std::string& path) { return path == "path"; }
+struct MockResource {
+    int value = -1;
+    bool loadFromFile(const std::string& path, int v = 2) {
+        if (path != "path") return false;
+        value = v;
+        return true;
+    }
 };
 
 TEST(ResourceTests, Test_LoadResource) {
-    ResourceManager<DemoResource> manager;
+    ResourceManager<MockResource> manager;
     ASSERT_TRUE(manager.load(0, "path"));
     ASSERT_FALSE(manager.load(1, "not_a_path"));
     ASSERT_FALSE(manager.load(0, "path"));
 }
 
 TEST(ResourceTests, Test_GetResource) {
-    ResourceManager<DemoResource> manager;
-    manager.load(0, "path");
+    ResourceManager<MockResource> manager;
+    manager.load(0, "path", 3);
 
     ASSERT_NE(manager.get(0), nullptr);
     ASSERT_EQ(manager.get(1), nullptr);
+    ASSERT_EQ(manager.get(0)->value, 3);
 }
