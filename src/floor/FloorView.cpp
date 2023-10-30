5cf48954dfd6a0f4ae7fdb889c45174c1eca15ed
u21434809 <u21434809@tuks.co.za>
2023-10-30 23:21:45 +0200

    added some sprites

--- src/floor/FloorView.cpp
+++ src/floor/FloorView.cpp
@@ -6,14 +6,30 @@ FloorView::FloorView(ResourceManager<sf::Texture>& textures)
 }
 
 void FloorView::draw(sf::RenderTarget& window) const {
-    sf::Sprite sprite(*textures_.get(0));
+    sf::Sprite floor(*textures_.get(0));
+    sf::Sprite entrance(*textures_.get(1));
+    sf::Sprite waiter(*textures_.get(2));
+    sf::Sprite wall(*textures_.get(3));
+
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < 10; j++) {
-            sprite.setPosition(i * 32 * 4, j * 32 * 4);
-            sprite.setScale(4, 4);
-            window.draw(sprite);
+            floor.setPosition(i * 32 * 3, j * 32 * 3);
+            floor.setScale(3, 3);
+            window.draw(floor);
+        }
+        for (int j = 0; j < 2; j++) {
+            wall.setPosition(i * 32 * 3, j * 32 * 3);
+            wall.setScale(3, 3);
+            window.draw(wall);
         }
     }
+    entrance.setPosition(0, 0);
+    entrance.setScale(2.7, 2.7);
+    window.draw(entrance);
+
+    waiter.setPosition(32 * 2, 32 * 2);
+    waiter.setScale(3, 3);
+    // window.draw(waiter);
 }
 sf::FloatRect FloorView::getBoundingBox() const {
     return sprite_.getGlobalBounds();
