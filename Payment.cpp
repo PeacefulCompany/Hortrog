9d30e9bbf17f6ff0e619f5c8a7932c80190aba5e
Kezzy04 <135063270+Kezzy04@users.noreply.github.com>
2023-11-03 19:29:56 +0200

    Ammended payment class files
    Allows for cash/card payments(still needs to be organised and split further)

--- Payment.cpp
+++ Payment.cpp
@@ -1,15 +1,70 @@
 #include "Payment.h"
 
-Payment::Payment(double amount) : amount(amount) {}
+Payment::Payment() : amountOfPayment(0) {}
 
-double Payment::calculateTotal() {
-    return amount;
+Payment::Payment(float amount) : amountOfPayment(amount) {}
+
+float Payment::getAmountOfPayment() const {
+    return amountOfPayment;
 }
 
-void Payment::makePayment(double amount) {
-    // Update the payment amount
-    this->amount -= amount;
-    if (this->amount < 0) {
-        this->amount = 0;
-    }
+void Payment::setAmountOfPayment(float amount) {
+    amountOfPayment = amount;
+}
+
+void Payment::paymentDetails() const {
+    std::cout << "Amount of payment: $" << amountOfPayment << std::endl;
+}
+
+CashPayment::CashPayment() : Payment(), cashPaid(0) {}
+
+CashPayment::CashPayment(float amount, float cashPaid) : Payment(amount), cashPaid(cashPaid) {}
+
+float CashPayment::getCashPaid() const {
+    return cashPaid;
+}
+
+void CashPayment::setCashPaid(float cashPaid) {
+    this->cashPaid = cashPaid;
+}
+
+void CashPayment::paymentDetails() const {
+    std::cout << "Amount of payment: $" << getAmountOfPayment() << std::endl;
+    std::cout << "Cash paid: $" << cashPaid << std::endl;
+}
+
+CreditCardPayment::CreditCardPayment() : Payment(), nameOnCard(""), expirationDate(""), creditCardNumber("") {}
+
+CreditCardPayment::CreditCardPayment(float amount, const std::string& nameOnCard, const std::string& expirationDate, const std::string& creditCardNumber)
+    : Payment(amount), nameOnCard(nameOnCard), expirationDate(expirationDate), creditCardNumber(creditCardNumber) {}
+
+const std::string& CreditCardPayment::getNameOnCard() const {
+    return nameOnCard;
+}
+
+const std::string& CreditCardPayment::getExpirationDate() const {
+    return expirationDate;
+}
+
+const std::string& CreditCardPayment::getCreditCardNumber() const {
+    return creditCardNumber;
+}
+
+void CreditCardPayment::setNameOnCard(const std::string& nameOnCard) {
+    this->nameOnCard = nameOnCard;
+}
+
+void CreditCardPayment::setExpirationDate(const std::string& expirationDate) {
+    this->expirationDate = expirationDate;
+}
+
+void CreditCardPayment::setCreditCardNumber(const std::string& creditCardNumber) {
+    this->creditCardNumber = creditCardNumber;
+}
+
+void CreditCardPayment::paymentDetails() const {
+    std::cout << "Amount of payment: $" << getAmountOfPayment() << std::endl;
+    std::cout << "Name on card: " << nameOnCard << std::endl;
+    std::cout << "Expiration date: " << expirationDate << std::endl;
+    std::cout << "Credit card number: " << creditCardNumber << std::endl;
 }
--- Payment.h
+++ Payment.h
@@ -1,15 +1,42 @@
-#ifndef PAYMENT_H
-#define PAYMENT_H
+#pragma once
+#include <iostream>
+#include <string>
 
-#include "Bill.h"
-
-class Payment : public Bill {
-private:
-    double amount;
+class Payment {
 public:
-    Payment(double amount);
-    double calculateTotal() override;
-    void makePayment(double amount) override;
+    Payment();
+    Payment(float amount);
+    float getAmountOfPayment() const;
+    void setAmountOfPayment(float amount);
+    virtual void paymentDetails() const;
+private:
+    float amountOfPayment;
 };
 
-#endif
+class CashPayment : public Payment {
+public:
+    CashPayment();
+    CashPayment(float amount, float cashPaid);
+    float getCashPaid() const;
+    void setCashPaid(float cashPaid);
+    void paymentDetails() const override;
+private:
+    float cashPaid;
+};
+
+class CreditCardPayment : public Payment {
+public:
+    CreditCardPayment();
+    CreditCardPayment(float amount, const std::string& nameOnCard, const std::string& expirationDate, const std::string& creditCardNumber);
+    const std::string& getNameOnCard() const;
+    const std::string& getExpirationDate() const;
+    const std::string& getCreditCardNumber() const;
+    void setNameOnCard(const std::string& nameOnCard);
+    void setExpirationDate(const std::string& expirationDate);
+    void setCreditCardNumber(const std::string& creditCardNumber);
+    void paymentDetails() const override;
+private:
+    std::string nameOnCard;
+    std::string expirationDate;
+    std::string creditCardNumber;
+};
