09164deb7d657ecd6e1ff71ba3f1269485c54f77
Kezzy04 <135063270+Kezzy04@users.noreply.github.com>
2023-11-03 19:40:17 +0200

    Amended main
    Waiter can interact with Point of sales to commit payments and create and amend the bills

--- main.cpp
+++ main.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
-#include "Bill.h"
+#include "PointOfSales.h"
 #include "Payment.h"
-#include "SubBill.h"
 
 int main() {
-    // Create a payment
-    Payment* payment1 = new Payment(100.0);
+    // Create a new PointOfSales object
+    PointOfSales pos;
 
-    // Create a sub-bill with two payments
-    SubBill* subBill1 = new SubBill("Sub-bill 1");
-    subBill1->addChild(new Payment(50.0));
-    subBill1->addChild(new Payment(75.0));
+    // Add a new bill for table 1, item "Pizza", price $10.00
+    Payment payment1(5.00);
+    pos.addBill(1, "Pizza", 10.00, payment1);
 
-    // Create another sub-bill with a payment and the previous sub-bill
-    SubBill* subBill2 = new SubBill("Sub-bill 2");
-    subBill2->addChild(payment1);
-    subBill2->addChild(subBill1);
+    // Add a new bill for table 2, item "Burger", price $8.00
+    Payment payment2(3.00);
+    pos.addBill(2, "Burger", 8.00, payment2);
 
-    // Add the sub-bills to a main bill
-    SubBill* mainBill = new SubBill("Main bill");
-    mainBill->addChild(subBill2);
-    mainBill->addChild(new Payment(25.0));
+    // Add a new bill for table 1, item "Soda", price $2.00
+    Payment payment3(2.00);
+    pos.addBill(1, "Soda", 2.00, payment3);
 
-    // Calculate the total amount due
-    double total = mainBill->calculateTotal();
-    std::cout << "Total amount due: R" << total << std::endl;
+    // Get the bill for table 1 and print the items and prices
+    SubBill* bill1 = pos.getBill(1);
+    std::cout << "Table 1 Bill:" << std::endl;
+    for (auto child : bill1->getChildren()) {
+        std::cout << "    " << child->getName() << " - $" << child->getPrice() << std::endl;
+    }
 
-    // Make a payment of R50
-    payment1->makePayment(50.0);
+    /* Remove the bill for table 2 and print the remaining bills
+    pos.removeBill(2, "Burger");
+    std::cout << "Remaining Bills:" << std::endl;
+    for (auto sb : pos.getBill()) {
+        std::cout << "Table " << sb->getTableNumber() << " - Balance: $" << sb->getBalance() << std::endl;
+    }*/
 
-    // Calculate the new total amount due
-    total = mainBill->calculateTotal();
-    std::cout << "Total amount due: R" << total << std::endl;
-
+    // Make a payment of $3.00 on the bill for table 1 and print the remaining balance
+    Payment payment4(3.00);
+    bill1->makePayment(payment4);
+    std::cout << "Table 1 Balance: $" << bill1->getBalance() << std::endl;
 
     return 0;
 }
