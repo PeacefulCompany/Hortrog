522f389fa0c85dbd3f6d8011750c9b6d87572996
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-11-04 18:16:29 +0200

    Added tblId

--- src/order/Order.h
+++ src/order/Order.h
@@ -6,8 +6,11 @@
 class Order{
 private:
     std::string customer;
+    int tblId;
 public:
     virtual void add(std::unique_ptr<Order>) = 0;
     virtual std::string toJson() = 0;
     virtual double total() = 0;
+    virtual void setTblId(int id){this->tblId = id;};
+    virtual int getTblId(){return tblId;};
 };
