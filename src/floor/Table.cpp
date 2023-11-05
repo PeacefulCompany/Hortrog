9ac0e2029b9e985d594c625f95fc01132426285e
Vincent Feistel <vafdaf11@gmail.com>
2023-11-05 16:03:12 +0200

    Revert "Fix/compile errors2"

--- src/floor/Table.cpp
+++
@@ -1,6 +0,0 @@
-#include "Table.h"
-
-#include "CustomerIterator.h"
-#include "floor/Table.h"
-
-CustomerIterator* Table::createIterator() { return new CustomerIterator(this); }
--- src/order/Receipt.cpp
+++
@@ -1,7 +0,0 @@
-#include "Receipt.h"
-std::vector<std::pair<std::string, double>> Receipt::getOrders() {
-    return orderList_;
-}
-void Receipt::generateOrders(Order* order) {
-    orderList_ = order->generateReceiptOrderList();
-}
