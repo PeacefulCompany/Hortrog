79837da54866fcbf4d781fe3e314d27e5c501d5d
Vincent <vafdaf11@gmail.com>
2023-11-06 12:20:47 +0200

    Replaced make_unique with nullptr

--- src/subsystem/Meals/Meal.h
+++ src/subsystem/Meals/Meal.h
@@ -15,7 +15,7 @@
 class Meal {
 
 private:
-    std::unique_ptr<Order> order_ = std::make_unique<Order>();
+    std::unique_ptr<Order> order_ = nullptr;
     std::vector<MealItem*> items_;
     bool ready_ = false;
     uint32_t tableId_;
