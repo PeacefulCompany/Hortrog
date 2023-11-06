e986989d2487abebd03d8f9c4f39d6a1c074725f
Michael <michael@horwitz.co.za>
2023-11-06 16:30:08 +0200

    Implemented PerCustomer

--- src/billing/PerCustomer.cpp
+++ src/billing/PerCustomer.cpp
@@ -1,5 +1,16 @@
-//
-// Created by michael on 2023/11/06.
-//
-
 #include "PerCustomer.h"
+#include <map>
+#include <set>
+std::vector<Receipt> PerCustomer::getReceipts(std::vector<Order*> orders) {
+    std::map<std::string, Receipt> map;
+    for (auto order : orders) {
+        Receipt receipt;
+        receipt.generateOrders(order);
+        map[order->getCustomer()].appendReceipt(&receipt);
+    }
+    std::vector<Receipt> returnVector;
+    for (auto pair : map) {
+        returnVector.push_back(pair.second);
+    }
+    return returnVector;
+}
--- src/billing/PerCustomer.h
+++ src/billing/PerCustomer.h
@@ -1,3 +1,5 @@
 #pragma once
 #include "POSStrategy.h"
-class PerCustomer : public POSStrategy{};
+class PerCustomer : public POSStrategy{
+    std::vector<Receipt> getReceipts(std::vector<Order*> orders) override;
+};
