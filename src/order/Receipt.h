c0c02f154a9dd48cc4ef4a53cee6795d2d8ce624
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-11-06 18:51:15 +0200

    Added doxy to Receipt

--- src/order/Receipt.h
+++ src/order/Receipt.h
@@ -7,6 +7,14 @@ private:
     std::vector<std::pair<std::string, double>> orderList_;
 
 public:
+    /**
+     * @brief returns the order list member
+     * @return a vector of pairs of key and price
+     */
     std::vector<std::pair<std::string, double>> getOrders();
+    /**
+     * @brief generates a vector of pairs of key words and price
+     * @param order the order from which the vector is generated
+     */
     void generateOrders(Order* order);
 };
